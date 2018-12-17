aa3c0ce1b9ea1bcdb82c296381adfa9854f94a95
Kennedy Mbogo <mbogokennedy@gmail.com>
2018-12-18 02:15:22 +0300

    changes on Js

--- js/app.js
+++ js/app.js
@@ -1,87 +1,100 @@
 // WOrking With DOM by Kennedy Mbogo
 var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
 var filter = document.getElementById('filter');
 
-// form submit event
 
-form.addEventListener('submit', addItem);
-// delete event
-itemList.addEventListener('click', removeItem);
-// filter event
-filter.addEventListener('keyup', filterItem);
-// add item
+form.addEventListener('submit', saveItem);
+submitBtn = document.querySelector('#submitBtn');
 
-function addItem(e) {
+submitBtn.disabled = true;
+//Restrict individual from submiting an empty form
+document.getElementById('shoppingItem').onkeyup = () => {
+    if (document.getElementById('shoppingItem').value.length > 0) {
+        submitBtn.disabled = false
+    } else {
+        submitBtn.disabled = true
+    };
+
+};
+
+//save item to the local storage
+function saveItem(e) {
     e.preventDefault();
-
-    // Get input value
     var shoppingItem = document.getElementById('shoppingItem').value;
     var category = document.getElementById('category').value;
+    var amount = document.getElementById('amount').value;
+    var itemList = { "shoppingItem": shoppingItem, "category": category, "amount": amount };
+    if (!localStorage.getItem('itemListBucket')) {
+        var itemContainer = [];
+        itemContainer.push(itemList);
+        localStorage.setItem('itemListBucket', JSON.stringify(itemContainer));
+    } else {
+        var itemContainer = JSON.parse(localStorage.getItem("itemListBucket"));
+        itemContainer.push(itemList);
+        localStorage.setItem("itemListBucket", JSON.stringify(itemContainer));
+    }
+    document.getElementById('shoppingItem').value = "";
+    document.getElementById('category').value = "default";
+    document.getElementById('amount').value = "";
 
-    // create Li newitem
-
-    var li = document.createElement('li');
-    var li2 = document.createElement('li');
-    // Add Class
-    li.className = 'list-group-item';
-    li2.className = 'list-group-item';
-    // add a text node with input value
-    li.appendChild(document.createTextNode(shoppingItem));
-    li2.appendChild(document.createTextNode(category));
-    // add del
-    var deleteBtn = document.createElement('button');
-
-    // class to button
-    deleteBtn.className = 'mt-2 btn btn-danger btn-sm float-right delete';
-    // append btn text node 
-    deleteBtn.appendChild(document.createTextNode('X'));
-    // append button to li
-    // append li to list
-
-    var tr = document.createElement('tr');
-    var td = document.createElement('td');
-    var td1 = document.createElement('td');
-    var td2 = document.createElement('td');
-
-    td.appendChild(li);
-    td1.appendChild(li2);
-    td2.appendChild(deleteBtn)
-
-    tr.appendChild(td);
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-
-    itemList.appendChild(tr);
-}
-
-
-function removeItem(e) {
-    if (e.target.classList.contains('delete')) {
-        if (confirm('Are you sure you want to delete this Item?')) {
-            var tr = e.target.parentElement.parentElement;
-            itemList.removeChild(tr);
+    fetchItems();
+};
+//remove item froma storage
+function removeItem(shoppingItem) {
+    if (confirm('Are you sure you want to delete this Item?')) {
+        var itemContainer = JSON.parse(localStorage.getItem('itemListBucket'));
+        for (var i = 0; i < itemContainer.length; i++) {
+            if (itemContainer[i].shoppingItem == shoppingItem) {
+                itemContainer.splice(i, 1);
+            }
         }
     }
 
+    localStorage.setItem("itemListBucket", JSON.stringify(itemContainer));
+
+    fetchItems();
 }
+//fetch item from a local storage
+function fetchItems() {
+    var itemContainer = JSON.parse(localStorage.getItem('itemListBucket'));
+    var itemListContainer = document.getElementById('items');
+    itemListContainer.innerHTML = '';
+    for (var i = 0; i < itemContainer.length; i++) {
+        var shoppingItem = itemContainer[i].shoppingItem;
+        var category = itemContainer[i].category;
+        var amount = itemContainer[i].amount;
+        itemListContainer.innerHTML += '<tr>' +
+            '<td>' + shoppingItem + '</td>' +
+            '<td>' + category + '</td>' +
+            '<td>' + amount + '</td>' +
+            '<td>' + ' <a onclick="removeItem(\'' + shoppingItem + '\')" class="btn btn-md btn-danger float-right" href="#">Delete</a> ' + '</td>' +
+            '</tr>';
+    }
+};
 
-// Filter Iterm
+fetchItems()
 
-function filterItem(e) {
-    // convert to lowercase
+// Filter Item from the Local stotrage
+
+filter.onkeyup = function filterItem(e) {
     var text = e.target.value.toLowerCase();
-    // get lis
 
-    var items = itemList.querySelectorAll('.list-group-item');
+    var itemContainer = JSON.parse(localStorage.getItem('itemListBucket'));
 
-    // convert to an array
-    Array.from(items).forEach(function(item) {
-        var itemName = item.firstChild.textContent;
+    var itemListContainer = document.getElementById('items');
+
+    itemListContainer.innerHTML = '';
+
+    Array.from(itemContainer).forEach(function(item) {
+        var itemName = item.shoppingItem;
+
         if (itemName.toLowerCase().indexOf(text) != -1) {
-            item.style.visibility = 'visible';
-        } else {
-            item.style.visibility = 'hidden';
+            itemListContainer.innerHTML += '<tr>' +
+                '<td>' + item.shoppingItem + '</td>' +
+                '<td>' + item.category + '</td>' +
+                '<td>' + item.amount + '</td>' +
+                '<td>' + ' <a onclick="removeItem(\'' + shoppingItem + '\')" class="btn btn-md btn-danger float-right" href="#">Delete</a> ' + '</td>' +
+                '</tr>';
         }
     })
 }
